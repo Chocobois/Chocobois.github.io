1122b3d691ff9e10e7a66457403c5f31bf8f683f
Dan-Eric Næss <dan-eric.naess@live.no>
2023-02-16 00:44:39 +0100

    Just testing something

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,6 +1,6 @@
 import { NavBar } from "../components/NavBar";
 import { GameCard } from "../components/GameCard";
-
+import { Fireworks } from '@fireworks-js/react'
 import games from '../data/games.json';
 
 export default function Games() {
@@ -13,6 +13,15 @@ export default function Games() {
                     <GameCard game={game} />
                 )}
             </div>
+            <Fireworks 
+                options={{
+                rocketsPoint: {
+                    min: 0,
+                    max: 100,
+                }
+                }}
+                className="top-0 left-0 w-full h-full fixed pointer-events-none">
+            </Fireworks>
         </div>
     </>);
 }
